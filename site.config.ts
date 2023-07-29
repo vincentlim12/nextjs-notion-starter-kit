da70b9dba98a67cccc2d7240b6f55a4d8b23bad2
vincentlim12 <140737871+vincentlim12@users.noreply.github.com>
2023-07-30 00:46:18 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '768e293d34944d88b29cc07c02eee012',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '米房大厅',
+  domain: 'https://catnip-wilderness-76d.notion.site/768e293d34944d88b29cc07c02eee012',
+  author: '云火',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
